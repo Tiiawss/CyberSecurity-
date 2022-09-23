11e63f4c799937422abfb6aff0d17538cafc9ede
Tiiawss <tiia.nylen@gmail.com>
2022-09-23 12:56:17 +0300

    Added table to frisbeegolf courses

--- schema.sql
+++ schema.sql
@@ -1,2 +1,3 @@
 CREATE TABLE visitors (id SERIAL PRIMARY KEY, time TIMESTAMP);
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
+CREATE TABLE courses (id SERIAL PRIMARY KEY, name TEXT, par INT, lenght INT, holes INT, adress TEXT);

7b1bc4d537bfb615fd23d0a43b3dab14db6b1901
Tiiawss <tiia.nylen@gmail.com>
2022-09-23 10:52:16 +0300

    user psql table added and passwords hasd

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import redirect, render_template, request, session
+from werkzeug.security import check_password_hash, generate_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
@@ -21,6 +22,22 @@ def login():
     username = request.form["username"]
     password = request.form["password"]
     # TODO: check username and password
+    hash_value = generate_password_hash(password)
+    sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
+    db.session.execute(sql, {"username":username, "password":hash_value})
+    db.session.commit()
+    sql = "SELECT id, password FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()    
+    #if not user:
+    # TODO: invalid username sign in 
+    #else:
+    hash_value = user.password
+    #if check_password_hash(hash_value, password):
+        # TODO: correct username and password
+        #else:
+        # TODO: invalid password
+
     session["username"] = username
     return redirect("/")
 

bae5a3d8f05b540b6630d83a63c60812f2859b2b
Tiiawss <tiia.nylen@gmail.com>
2022-09-23 10:24:24 +0300

    Created a login

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
-from flask import redirect, render_template
+from flask import redirect, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.secret_key = getenv("SECRET_KEY")
 db = SQLAlchemy(app)
 
 @app.route("/")
@@ -14,3 +15,16 @@ def index():
     result = db.session.execute("SELECT COUNT(*) FROM visitors")
     counter = result.fetchone()[0]
     return render_template("index.html", counter=counter) 
+
+@app.route("/login",methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    # TODO: check username and password
+    session["username"] = username
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")

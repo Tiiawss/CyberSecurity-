0773c296ed278a9ee5a9278d6f631278cacee31e
Tiiawss <tiia.nylen@gmail.com>
2022-10-16 15:22:52 +0300

    Adding the basics to begin drafting the possibility of rewieving the courses

--- routes.py
+++ routes.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask import redirect, render_template, request, session
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_sqlalchemy import SQLAlchemy
+from db import db
 from os import getenv
 import courses
 import visits
@@ -10,11 +11,87 @@ import login
 
 
 
-@app.route("/")
-def index():
+@app.route("/visitors")
+def visitors():
     visits.add_visit()
     counter = visits.get_counter()
     return render_template("index.html", counter=counter)
+
+@app.route("/")
+def index():
+    sql = "SELECT id, course_name, created_at FROM polls ORDER BY id DESC"
+    result = db.session.execute(sql)
+    polls = result.fetchall()
+    return render_template("index.html", polls=polls)
+
+@app.route("/new")
+def new():
+    return render_template("new.html")
+
+@app.route("/create", methods=["POST"])
+def create():
+    topic = request.form["name"]
+    #sql = "INSERT INTO polls (topic, created_at) VALUES (:topic, NOW()) RETURNING id"
+    #result = db.session.execute(sql, {"topic":topic})
+
+
+    poll_id = result.fetchone()[0]
+    choices = request.form.getlist("choice")
+    course_name= request.form["name"]
+    par = request.form["par"]
+    lenght = request.form["lenght"]
+    holes =request.form["holes"]
+    city= request.form["city"]
+    postcode=request.form["postnumber"]
+    adress=request.form["adress"]
+
+    """
+    sql = "INSERT INTO courses (course_name, par, lenght, holes, city, postcode, adress) VALUES (:course_name, :par, :lenght, :holes, :city, :postcode, :adress)"
+    db.session.execute(sql, {"course_name":course_name, "par":par, "lenght":lenght, "holes":holes, "city":city, "postcode":postcode, "adress":adress})
+    """
+    sql = "INSERT INTO polls (course_name, par, lenght, holes, city, postcode, adress, topic, created_at) VALUES (:course_name, :par, :lenght, :holes, :city, :postcode, :adress, :topic, NOW()) RETURNING id"
+    db.session.execute(sql, {"course_name":course_name, "par":par, "lenght":lenght, "holes":holes, "city":city, "postcode":postcode, "adress":adress, "topic":topic})
+    """for choice in choices:
+        if choice != "":
+            sql = "INSERT INTO choices (poll_id, choice) VALUES (:poll_id, :choice)"
+            db.session.execute(sql, {"poll_id":poll_id, "choice":choice})
+    """
+    db.session.commit()
+
+    return redirect("/")
+
+@app.route("/poll/<int:id>")
+def poll(id):
+    sql = "SELECT topic FROM polls WHERE id=:id"
+    result = db.session.execute(sql, {"id":id})
+    topic = result.fetchone()[0]
+    sql = "SELECT id, choice FROM choices WHERE poll_id=:id"
+    result = db.session.execute(sql, {"id":id})
+    choices = result.fetchall()
+    return render_template("poll.html", id=id, topic=topic, choices=choices)
+
+@app.route("/answer", methods=["POST"])
+def answer():
+    poll_id = request.form["id"]
+    if "answer" in request.form:
+        choice_id = request.form["answer"]
+        sql = "INSERT INTO answers (choice_id, sent_at) VALUES (:choice_id, NOW())"
+        db.session.execute(sql, {"choice_id":choice_id})
+        db.session.commit()
+    return redirect("/result/" + str(poll_id))
+
+@app.route("/result/<int:id>")
+def result(id):
+    sql = "SELECT topic FROM polls WHERE id=:id"
+    result = db.session.execute(sql, {"id":id})
+    topic = result.fetchone()[0]
+    sql = "SELECT c.choice, COUNT(a.id) FROM choices c LEFT JOIN answers a " \
+          "ON c.id=a.choice_id WHERE c.poll_id=:poll_id GROUP BY c.id"
+    result = db.session.execute(sql, {"poll_id":id})
+    choices = result.fetchall()
+    return render_template("result.html", topic=topic, choices=choices)
+
+
 
 @app.route("/login",methods=["POST"])
 def login1():
@@ -46,14 +123,14 @@ def logout():
     del session["username"]
     return redirect("/")
 
-@app.route("/order")
+"""@app.route("/order")
 def order():
-    return render_template("index.html")
+    return render_template("index.html")"""
 
 
-
+"""
 @app.route("/result", methods=["POST"])
-def result():
+def result2():
     course_name= request.form["course_name"]
     par = request.form["par"]
     lenght = request.form["lenght"]
@@ -66,4 +143,4 @@ def result():
     #sql = "INSERT INTO courses (course_name, par, lenght, holes, city, postcode, adress) VALUES (:course_name, :par, :lenght, :holes, :city, :postcode, :adress)"
     #db.session.execute(sql, {"course_name":course_name, "par":par, "lenght":lenght, "holes":holes, "city":city, "postcode":postcode, "adress":adress})
     #db.session.commit()
-    return redirect("/")
+    return redirect("/")"""
--- schema.sql
+++ schema.sql
@@ -1,3 +1,6 @@
 CREATE TABLE visitors (id SERIAL PRIMARY KEY, time TIMESTAMP);
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
-CREATE TABLE courses (id SERIAL PRIMARY KEY, course_name TEXT, par INT, lenght INT, holes INT, city TEXT, postcode INT, adress TEXT);
+CREATE TABLE courses (id SERIAL PRIMARY KEY, course_name TEXT, par INT, lenght INT, holes INT, city TEXT, postcode INT, adress TEXT, created_at TIMESTAMP);
+CREATE TABLE polls (id SERIAL PRIMARY KEY,topic TEXT,created_at TIMESTAMP);
+CREATE TABLE choices (id SERIAL PRIMARY KEY,poll_id INTEGER REFERENCES polls,choice TEXT);
+CREATE TABLE answers (id SERIAL PRIMARY KEY,choice_id INTEGER REFERENCES choices,sent_at TIMESTAMP);

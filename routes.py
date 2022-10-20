988ecbcac863da1be4a7154b42050eacec589b1c
Tiiawss <tiia.nylen@gmail.com>
2022-10-20 14:36:59 +0300

    Changed the htlm to have one layout

--- routes.py
+++ routes.py
@@ -5,9 +5,12 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from db import db
 from os import getenv
-import courses
+import course_app
 import visits
 import login
+import ratings
+import users
+
 
 
 
@@ -19,10 +22,11 @@ def visitors():
 
 @app.route("/")
 def index():
-    sql = "SELECT id, course_name, created_at FROM polls ORDER BY id DESC"
-    result = db.session.execute(sql)
-    polls = result.fetchall()
-    return render_template("index.html", polls=polls)
+    visits.add_visit()
+    counter = visits.get_counter()
+    courses= course_app.index_course()
+    
+    return render_template("index.html", courses=courses, counter=counter)
 
 @app.route("/new")
 def new():
@@ -30,13 +34,8 @@ def new():
 
 @app.route("/create", methods=["POST"])
 def create():
-    topic = request.form["name"]
-    #sql = "INSERT INTO polls (topic, created_at) VALUES (:topic, NOW()) RETURNING id"
-    #result = db.session.execute(sql, {"topic":topic})
-
-
-    poll_id = result.fetchone()[0]
-    choices = request.form.getlist("choice")
+    
+  
     course_name= request.form["name"]
     par = request.form["par"]
     lenght = request.form["lenght"]
@@ -45,102 +44,74 @@ def create():
     postcode=request.form["postnumber"]
     adress=request.form["adress"]
 
-    """
-    sql = "INSERT INTO courses (course_name, par, lenght, holes, city, postcode, adress) VALUES (:course_name, :par, :lenght, :holes, :city, :postcode, :adress)"
-    db.session.execute(sql, {"course_name":course_name, "par":par, "lenght":lenght, "holes":holes, "city":city, "postcode":postcode, "adress":adress})
-    """
-    sql = "INSERT INTO polls (course_name, par, lenght, holes, city, postcode, adress, topic, created_at) VALUES (:course_name, :par, :lenght, :holes, :city, :postcode, :adress, :topic, NOW()) RETURNING id"
-    db.session.execute(sql, {"course_name":course_name, "par":par, "lenght":lenght, "holes":holes, "city":city, "postcode":postcode, "adress":adress, "topic":topic})
-    """for choice in choices:
-        if choice != "":
-            sql = "INSERT INTO choices (poll_id, choice) VALUES (:poll_id, :choice)"
-            db.session.execute(sql, {"poll_id":poll_id, "choice":choice})
-    """
-    db.session.commit()
+    course_app.add_course(course_name, par, lenght, holes, city, postcode, adress)
+    return redirect("/")
+@app.route("/review/<int:id>")
+def review(id):
+    
+    return render_template("review.html",id=id)
+
+@app.route("/create_review", methods=["POST"])
+def create_review():
+    
+  
+    rating= request.form["rating"]
+    course_id = request.form["id"]
+   
+    ratings.add_rating(rating,course_id)
+   
+    """sql = "INSERT INTO reviews (rating, course_id) VALUES (:rating, :course_id)"
+    db.session.execute(sql, {"rating":rating, "course_id":course_id})
+    sql= "INSERT INTO courses SELECT rating FROM reviews WHERE course_id"""
+
+    
+    
 
     return redirect("/")
 
-@app.route("/poll/<int:id>")
-def poll(id):
-    sql = "SELECT topic FROM polls WHERE id=:id"
-    result = db.session.execute(sql, {"id":id})
-    topic = result.fetchone()[0]
-    sql = "SELECT id, choice FROM choices WHERE poll_id=:id"
-    result = db.session.execute(sql, {"id":id})
-    choices = result.fetchall()
-    return render_template("poll.html", id=id, topic=topic, choices=choices)
-
-@app.route("/answer", methods=["POST"])
-def answer():
-    poll_id = request.form["id"]
-    if "answer" in request.form:
-        choice_id = request.form["answer"]
-        sql = "INSERT INTO answers (choice_id, sent_at) VALUES (:choice_id, NOW())"
-        db.session.execute(sql, {"choice_id":choice_id})
-        db.session.commit()
-    return redirect("/result/" + str(poll_id))
-
-@app.route("/result/<int:id>")
-def result(id):
-    sql = "SELECT topic FROM polls WHERE id=:id"
-    result = db.session.execute(sql, {"id":id})
-    topic = result.fetchone()[0]
-    sql = "SELECT c.choice, COUNT(a.id) FROM choices c LEFT JOIN answers a " \
-          "ON c.id=a.choice_id WHERE c.poll_id=:poll_id GROUP BY c.id"
-    result = db.session.execute(sql, {"poll_id":id})
-    choices = result.fetchall()
-    return render_template("result.html", topic=topic, choices=choices)
 
 
 
-@app.route("/login",methods=["POST"])
-def login1():
-    username = request.form["username"]
-    password = request.form["password"]
-    # TODO: check username and password
-    hash_value = generate_password_hash(password)
-    login.login2(username, hash_value)
-    #sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
-    #db.session.execute(sql, {"username":username, "password":hash_value})
-    #db.session.commit()
-    #sql = "SELECT id, password FROM users WHERE username=:username"
-    #result = db.session.execute(sql, {"username":username})
-    #user = result.fetchone()    
-    #if not user:
-    # TODO: invalid username sign in 
-    #else:
-    #hash_value = user.password
-    #if check_password_hash(hash_value, password):
-        # TODO: correct username and password
-        #else:
-        # TODO: invalid password
 
-    session["username"] = username
-    return redirect("/")
+
+
+@app.route("/login", methods=["get", "post"])
+def login():
+	if request.method == "GET":
+		return render_template("login.html")
+	if request.method == "POST":
+		username = request.form["username"]
+		password = request.form["password"]
+		if users.login(username, password):
+			return redirect("/")
+		else:
+			return render_template("error.html", message="Väärä käyttäjätunnus tai salasana")
+
+
+@app.route("/register", methods=["get", "post"])
+def register():
+	if request.method == "GET":
+		return render_template("register.html")
+
+	if request.method == "POST":
+		username = request.form["username"]
+		if len(username) < 1 or len(username) > 15:
+			return render_template("error.html", message="Käyttätunnuksen tulee olla 1-15 merkkiä pitkä")
+		password1 = request.form["password1"]
+		password2 = request.form["password2"]
+		if password1 != password2:
+			return render_template("error.html", message="Salasanat eroavat")
+		if password1 == "":
+			return render_template("error.html", message="Salasana ei voi olla tyhjä")
+		
+		role = request.form["role"]
+		if role not in ("1", "2"):
+			return render_template("error.html", message="Tuntematon käyttäjärooli")
+		if not users.register(username, password1, role):
+			return render_template("error.html", message="Rekisteröinti ei onnistunut")
+		return redirect("/") 
 
 @app.route("/logout")
 def logout():
-    del session["username"]
-    return redirect("/")
-
-"""@app.route("/order")
-def order():
-    return render_template("index.html")"""
-
-
-"""
-@app.route("/result", methods=["POST"])
-def result2():
-    course_name= request.form["course_name"]
-    par = request.form["par"]
-    lenght = request.form["lenght"]
-    holes =request.form["holes"]
-    city= request.form["city"]
-    postcode=request.form["postcode"]
-    adress=request.form["adress"]
-
-    courses.add_course(course_name, par, lenght, holes, city, postcode, adress)
-    #sql = "INSERT INTO courses (course_name, par, lenght, holes, city, postcode, adress) VALUES (:course_name, :par, :lenght, :holes, :city, :postcode, :adress)"
-    #db.session.execute(sql, {"course_name":course_name, "par":par, "lenght":lenght, "holes":holes, "city":city, "postcode":postcode, "adress":adress})
-    #db.session.commit()
-    return redirect("/")"""
+	users.logout()
+	return redirect("/")
--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 CREATE TABLE visitors (id SERIAL PRIMARY KEY, time TIMESTAMP);
-CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
-CREATE TABLE courses (id SERIAL PRIMARY KEY, course_name TEXT, par INT, lenght INT, holes INT, city TEXT, postcode INT, adress TEXT, created_at TIMESTAMP);
+CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT, name TEXT, role INTEGER);
+CREATE TABLE courses (id SERIAL PRIMARY KEY, course_name TEXT, par INT, lenght INT, holes INT, city TEXT, postcode INT, adress TEXT, created_at TIMESTAMP, rated INT);
 CREATE TABLE polls (id SERIAL PRIMARY KEY,topic TEXT,created_at TIMESTAMP);
 CREATE TABLE choices (id SERIAL PRIMARY KEY,poll_id INTEGER REFERENCES polls,choice TEXT);
 CREATE TABLE answers (id SERIAL PRIMARY KEY,choice_id INTEGER REFERENCES choices,sent_at TIMESTAMP);
+CREATE TABLE reviews (id SERIAL PRIMARY KEY, course_id INTEGER REFERENCES courses, rating INT);

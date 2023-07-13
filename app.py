db25a91410eadbc1b5366c868e905030541fdcd3
Tiiawss <tiia.nylen@gmail.com>
2023-07-13 21:12:33 +0300

    added link to front page

--- app.py
+++ app.py
@@ -4,5 +4,6 @@ from flask import Flask
 
 load_dotenv()
 app = Flask(__name__)
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {"pool_pre_ping": True} 
 app.secret_key = getenv('SECRET_KEY')
 import routes
